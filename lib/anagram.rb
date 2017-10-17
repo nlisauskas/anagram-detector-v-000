41b6c0eb16207a870b187f7a916f484e959a85a1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-17 02:23:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = []
+    array.each do |chance|
+      if chance.chars.sort.join == @word.chars.sort.join then new_array << chance end
+    end
+    new_array.uniq
+  end
+
+
+end
